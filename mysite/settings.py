a5ec3b0f9f85b36c781818d069ef9681717a0f64
Arik <arik.hamdi@gmail.com>
2020-06-30 19:29:39 +0200

    sharing post by email

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+# Console email. Only for test
+# Remove it if you are using an SMTP server 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
